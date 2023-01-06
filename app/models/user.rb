66a683989d130980dc81bf70c4667cd61f0776f2
miyazato1872 <miyazato5997@gmail.com>
2023-01-06 16:18:34 +0900

    新規登録、ログイン、ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,18 +10,14 @@ class User < ApplicationRecord
   validates :first_name, presence: true  #空では登録できない
   validates :kana_last_name, presence: true   #空では登録できない
   validates :kana_first_name, presence: true   #空では登録できない
-  validates :birth_date, presence: true
+  validates :birth_date, presence: true  #空では登録できない
 
-  validates_format_of  :last_name, with:  /\A[ぁ-んァ-ン一-龥]/  #全角（漢字・ひらがな・カタカナ）での入力が必須
-  validates_format_of  :first_name, with:  /\A[ぁ-んァ-ン一-龥]/  #全角（漢字・ひらがな・カタカナ）での入力が必須
+  validates_format_of :last_name, with: /\A[ぁ-んァ-ン一-龥]/   #全角（漢字・ひらがな・カタカナ）での入力が必須
+  validates_format_of :first_name, with: /\A[ぁ-んァ-ン一-龥]/   #全角（漢字・ひらがな・カタカナ）での入力が必須
 
-  validates_format_of  :last_name, with:  /\A[ァ-ヶー－]+\z/  #全角（カタカナ）での入力が必須
-  validates_format_of  :first_name, with:  /\A[ァ-ヶー－]+\z/  #全角（カタカナ）での入力が必須
-
+  validates_format_of  :kana_last_name, with:  /\A[ァ-ヶー－]+\z/  #全角（カタカナ）での入力が必須
+  validates_format_of  :kana_first_name, with:  /\A[ァ-ヶー－]+\z/  #全角（カタカナ）での入力が必須
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :encrypted_password, with:  VALID_PASSWORD_REGEX  #半角英数字混合での入力が必須
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む必要があります"}
 end
-
-
-
