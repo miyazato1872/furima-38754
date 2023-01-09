5f2c2e0c74e8488808ccb973dcc9d08c917664a2
miyazato1872 <miyazato5997@gmail.com>
2023-01-09 16:08:57 +0900

    Rubocopを実行し、check内容を自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true # 空では登録できない
+  validates :last_name, presence: true # 空では登録できない
+  validates :first_name, presence: true # 空では登録できない
+  validates :kana_last_name, presence: true # 空では登録できない
+  validates :kana_first_name, presence: true # 空では登録できない
+  validates :birth_date, presence: true # 空では登録できない
 
-  validates :nickname, presence: true  #空では登録できない
-  validates :last_name, presence: true  #空では登録できない
-  validates :first_name, presence: true  #空では登録できない
-  validates :kana_last_name, presence: true   #空では登録できない
-  validates :kana_first_name, presence: true   #空では登録できない
-  validates :birth_date, presence: true  #空では登録できない
+  validates_format_of :last_name, with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true # 全角（漢字・ひらがな・カタカナ）での入力が必須
+  validates_format_of :first_name, with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true # 全角（漢字・ひらがな・カタカナ）での入力が必須
 
-  validates_format_of :last_name, with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true   #全角（漢字・ひらがな・カタカナ）での入力が必須
-  validates_format_of :first_name, with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true   #全角（漢字・ひらがな・カタカナ）での入力が必須
+  validates_format_of  :kana_last_name, with: /\A[ァ-ヶー－]+\z/, allow_blank: true # 全角（カタカナ）での入力が必須
+  validates_format_of  :kana_first_name, with: /\A[ァ-ヶー－]+\z/, allow_blank: true # 全角（カタカナ）での入力が必須
 
-  validates_format_of  :kana_last_name, with:  /\A[ァ-ヶー－]+\z/, allow_blank: true  #全角（カタカナ）での入力が必須
-  validates_format_of  :kana_first_name, with:  /\A[ァ-ヶー－]+\z/, allow_blank: true  #全角（カタカナ）での入力が必須
-
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }, allow_blank: true  #半角英数字混合での入力が必須
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }, allow_blank: true  # 半角英数字混合での入力が必須
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 4)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    
+    nickname              { Faker::Name.initials(number: 4) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+
     transient do
       person { Gimei.name }
     end
