4b1407d5251e5fe19baddeab41fd2a5f31b6044b
miyazato1872 <miyazato5997@gmail.com>
2023-01-09 15:32:48 +0900

    テストコードの記述完了
    正常系、異常系
    合計18 examples

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :user do
+    nickname              {Faker::Name.initials(number: 4)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
     
+    transient do
+      person { Gimei.name }
+    end
+    first_name { person.first.kanji }
+    last_name { person.last.kanji }
+    kana_first_name { person.first.katakana }
+    kana_last_name { person.last.katakana }
+    birth_date { Faker::Date.backward }
   end
 end
