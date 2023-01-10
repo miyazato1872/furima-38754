6c610c5b5ad37ea5e1f4313e466a2a474da02399
miyazato1872 <miyazato5997@gmail.com>
2023-01-10 14:21:52 +0900

    new.html.erb内のプルダウンを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,28 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :area
+  belongs_to :days_to_ship
+
+
   belongs_to :user
   has_one_attached :image
+
+  validates :category_id, :text, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  
+  validates :status_id, :text, presence: true
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
+  
+  validates :shipping_charge_id, :text, presence: true
+  validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" } 
+  
+  validates :area_id, :text, presence: true
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" } 
+  
+  validates :days_to_ship_id, :text, presence: true
+  validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" } 
+  
 end
