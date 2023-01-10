83521e14b64f72c3addf0310d96ecb65bb66d6b3
miyazato1872 <miyazato5997@gmail.com>
2023-01-11 00:36:56 +0900

    Itemモデルのテストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,27 +10,23 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :image, presence: true
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :price, presence: true
+  validates :image,               presence: true
+  validates :name,                presence: true
+  validates :description,         presence: true
+  validates :category_id,         presence: true
+  validates :status_id,           presence: true
+  validates :shipping_charge_id,  presence: true
+  validates :area_id,             presence: true
+  validates :days_to_ship_id,     presence: true
+  validates :price,               presence: true
+
+  
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" } 
+
   validates_inclusion_of :price, in:300..9999999, message: "Price is invalid", allow_blank: true
   validates :price, format: { with: /\A[0-9]+\z/ }, allow_blank: true
-
-  validates :category_id, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
-  
-  validates :status_id, presence: true
-  validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
-  
-  validates :shipping_charge_id, presence: true
-  validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" } 
-  
-  validates :area_id, presence: true
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" } 
-  
-  validates :days_to_ship_id, presence: true
-  validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" } 
-  
-
 end
