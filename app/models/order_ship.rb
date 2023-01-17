6d4f5e8c46b473489418f035a1bab2b0e8e3c934
miyazato1872 <miyazato5997@gmail.com>
2023-01-17 13:25:42 +0900

    Formオブジェクト作成、バリデーション記述

--- app/models/order_ship.rb
+++ app/models/order_ship.rb
@@ -2,6 +2,15 @@ class OrderShip
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :area_id, :city, :address, :building, :phone, :order_id
 
-  
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: {with: /\A\d{3}[-]\d{4}\z/}
+    validates :area_id,     numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :phone, format: {with: /\A\d{10,11}\z/}
+    validates :order_id
+  end
 
 end
